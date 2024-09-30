cc1725a938ab7f8f2de5aadc70cc73f88fbb47da
juliegambale <julie.gambale@gmail.com>
2024-09-30 12:52:17 +0200

    new argument -> class, getter and setter

---
+++ src/EsempioCarta/MischiaCarte.java
@@ -0,0 +1,16 @@
+package EsempioCarta;
+
+public class MischiaCarte {
+
+    public Carta[] mischia(Carta[] mazzo) {
+        for (int i = 0; i < 100; i++) {
+            int random = Math.round((int) (Math.random() * (mazzo.length)));
+            int random1 = Math.round((int) (Math.random() * (mazzo.length)));
+            Carta temp = mazzo[random];
+            mazzo[random] = mazzo[random1];
+            mazzo[random1] = temp;
+        }
+        return mazzo;
+    }
+
+}
--- src/EsempioCarta/Prova.java
+++ src/EsempioCarta/Prova.java
@@ -1,5 +1,7 @@
 package EsempioCarta;
 
+import java.util.Arrays;
+
 public class Prova {
 
     public static void main(String[] args) {
@@ -13,22 +15,31 @@ public class Prova {
         carta2.setValore("2");
 
         Carta[] mazzo = new Carta[52];
-        String[] semi = {"Cuori", "Quadri", "Semi", "Picche"};
+        String[] semi = {"Cuori", "Quadri", "Fiori", "Picche"};
         String[] valori = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-        int seme = 0;
+        int index = 0;
 
-        for (int i = 0; i < mazzo.length; i++) {
-            for (int j = 0; j < mazzo.length / 13; j++) {
+        for (String seme : semi) {
+            for (String valore : valori) {
                 Carta cartaMazzo = new Carta();
-                cartaMazzo.setSeme(semi[seme]);
-                cartaMazzo.setValore(valori[j]);
-                mazzo[i] = cartaMazzo;
+                cartaMazzo.setSeme(seme);
+                cartaMazzo.setValore(valore);
+                mazzo[index] = cartaMazzo;
+                index++;
             }
         }
+
+        MischiaCarte michiaCarte = new MischiaCarte();
+        mazzo = michiaCarte.mischia(mazzo);
 
         System.out.println(carta.stringDellaCarta());
         System.out.println(carta2.stringDellaCarta());
 
-    }
+        System.out.println("--------- Cambio esempio ---------");
 
+        for (Carta c : mazzo) {
+            System.out.println(c.stringDellaCarta());
+        }
+
+    }
 }
